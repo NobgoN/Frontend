14f0a88407039fa5e7099ca8a49b5a41b68bd4ff
Kei0201 <ujlow886@gmail.com>
2023-09-07 22:08:29 +0900

    first commit

---
+++ src/components/FirstHello.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const props = defineProps({
+        message: String,
+        name: {type: String, default: 'jane'},
+        
+    });
+</script>
+
+<template>
+    Hello
+    <p> {{ props.message }} {{ props.name }}</p>
+</template>
